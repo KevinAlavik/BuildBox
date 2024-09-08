06c17803243090f18208ff85b037337d043bf045
Kevin Alavik <kevin@alavik.se>
2024-09-08 19:59:22 +0200

    feat: Created a simple argument handler

--- BuildBox/BuildBox.cpp
+++ BuildBox/BuildBox.cpp
@@ -1,7 +1,39 @@
 ﻿#include <BuildBox.hpp>
 
-int main()
-{
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+void showHelp() {
+    std::cout << BLUE_COLOR << "Usage: " << APP_NAME << " [options] <recipe>" << RESET_COLOR << std::endl;
+    std::cout << "\n" << BLUE_COLOR << "Version: " << APP_VERSION << "\n" << APP_DESC << "\n\n" << RESET_COLOR;
+
+    std::cout << YELLOW_COLOR << "Options:\n" << RESET_COLOR;
+    std::cout << "  -h, --help       Display this help message\n";
+    std::cout << "  -v, --version    Show version information\n";
+}
+
+void showVersion() {
+    std::cout << APP_NAME << " version: " << APP_VERSION << std::endl;
+}
+
+int main(int argc, char** argv) {
+    if (argc == 1) {
+        std::cerr << RED_COLOR << "Error: No arguments provided. Usage:\n  " << argv[0] << " [options] <recipe>\n" <<
+            "For more information, use -h or --help." << RESET_COLOR << std::endl;
+        return 1;
+    }
+
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg == "-h" || arg == "--help") {
+            showHelp();
+            return 0;
+        }
+        if (arg == "-v" || arg == "--version") {
+            showVersion();
+            return 0;
+        }
+    }
+
+    std::string recipe = argv[argc - 1];
+    std::cout << "Processing recipe: " << recipe << std::endl;
+
+    return 0;
 }
--- BuildBox/BuildBox.hpp
+++ BuildBox/BuildBox.hpp
@@ -1,5 +1,17 @@
 ﻿#pragma once
 
+// Standard library
 #include <iostream>
 
-#include <core/ArgParser.hpp>
+// Utility defines
+#define RESET_COLOR "\033[0m"
+#define RED_COLOR "\033[31m"
+#define GREEN_COLOR "\033[32m"
+#define YELLOW_COLOR "\033[33m"
+#define BLUE_COLOR "\033[34m"
+
+// Application metadata
+const std::string APP_NAME = "BuildBox";
+const std::string APP_DESC = "Cross platform multi purpose build system.";
+const std::string APP_VERSION = "1.0.0-alpha";
+
